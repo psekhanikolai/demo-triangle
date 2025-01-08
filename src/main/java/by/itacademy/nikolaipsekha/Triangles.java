ceb36851cb8a33a171ced5c2ebbd2483a1dcdd09
Nikolai <n91056379@gmail.com>
2025-01-08 11:05:41 +0300

    add method not Triangle

--- src/main/java/by/itacademy/nikolaipsekha/Triangles.java
+++ src/main/java/by/itacademy/nikolaipsekha/Triangles.java
@@ -1,4 +1,9 @@
 package by.itacademy.nikolaipsekha;
 
 public class Triangles {
-}
+    public static String defineTriangle(int a, int b, int c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            return "Треугольник a=..., b=..., c=... не является треугольником";
+        }
+
+    }
