f763b05643b6d961f35192ef361cf16ce3b10b8b
Nikolai <n91056379@gmail.com>
2025-01-08 12:18:05 +0300

    set all method треугольник

--- src/main/java/by/itacademy/nikolaipsekha/Triangles.java
+++ src/main/java/by/itacademy/nikolaipsekha/Triangles.java
@@ -3,18 +3,18 @@ package by.itacademy.nikolaipsekha;
 public class Triangles {
     public static String defineTriangle(int a, int b, int c) {
         if (a + b < c || a + c < b || b + c < a) {
-            return "Треугольник a=..., b=..., c=... не является треугольником";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c +  "не является треугольником";
         }
         if (a == b & b == c) {
-            return "Треугольник a=..., b=..., c=... - равносторонний треугольник";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + "- равносторонний треугольник";
 
         } else if (a == b || a == c || b == c) {
             return "Треугольник a=..., b=..., c=... - равнобедренный треугольник";
         } else if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
-            return "Треугольник a=..., b=..., c=... - прямогульный треугольник";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + " - прямогульный треугольник";
         }
         else
-            return "Треугольник a=..., b=..., c=... - обычный треугольник";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + " - обычный треугольник";
         }
     }
 
