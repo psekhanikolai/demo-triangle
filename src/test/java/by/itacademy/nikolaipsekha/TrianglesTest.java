d11ff4d47e064da696a0c08a9e3856fb3d41eae6
Nikolai <n91056379@gmail.com>
2025-01-08 12:54:48 +0300

    add all testMethod triangles

--- src/test/java/by/itacademy/nikolaipsekha/TrianglesTest.java
+++ src/test/java/by/itacademy/nikolaipsekha/TrianglesTest.java
@@ -4,11 +4,57 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TrianglesTest {
-    public String expectedTriangle( int a, int b, int c, String name){
-        return "Треугольник a=" + a + ", b= " + b +  ", c= " + c +  " - " + name;
+    public String expectedTriangle(int a, int b, int c, String name) {
+        return "Треугольник a=" + a + ", b= " + b + ", c= " + c + " - " + name;
     }
-@Test
-    public void testDefineTriangle1(){
-    Assertions.assertEquals(expectedTriangle(2,2,2,"равносторонний треугольник") , Triangles.defineTriangle(2,2,2));
+
+    @Test
+    public void testDefineTriangle1() {
+        Assertions.assertEquals(expectedTriangle(2, 2, 2, "равносторонний треугольник"), Triangles.defineTriangle(2, 2, 2));
+
+    }
+
+    @Test
+    public void testDefineTriangle2() {
+        Assertions.assertEquals(expectedTriangle(2, 2, 4, "равнобедренный треугольник"), Triangles.defineTriangle(2, 2, 4));
+
+    }
+
+    @Test
+    public void testDefineTriangle3() {
+        Assertions.assertEquals(expectedTriangle(2,7,6, "обычный треугольник"), Triangles.defineTriangle(2, 7, 6));
+
+    }
+
+    @Test
+    public void testDefineTriangle4() {
+        Assertions.assertEquals(expectedTriangle(3, 4, 5, "прямоугольный треугольник"), Triangles.defineTriangle(3, 4, 5));
+
+    }
+
+    @Test
+    public void testDefineTriangle5() {
+        Assertions.assertEquals(expectedTriangle(2, 2, -2, "не является треугольником"), Triangles.defineTriangle(2, 2, -2));
+
+    }
+
+    @Test
+    public void testDefineTriangle6() {
+        Assertions.assertEquals(expectedTriangle(7, 7, 7, "равносторонний треугольник"), Triangles.defineTriangle(7, 7, 7));
+
+    }
+
+    @Test
+    public void testDefineTriangle7() {
+        Assertions.assertEquals(expectedTriangle(20, 22, 20, "равнобедренный треугольник"), Triangles.defineTriangle(20, 22, 20));
+
+    }
+
+    @Test
+    public void testDefineTriangle8() {
+        Assertions.assertEquals(expectedTriangle(0, 2, 0, "не является треугольником"), Triangles.defineTriangle(0, 2, 0));
+
+    }
+
 }
-}
+
