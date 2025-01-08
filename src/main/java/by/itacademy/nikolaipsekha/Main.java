f94d967c2089c83071f166f0b689c8d48fe9227c
Nikolai <n91056379@gmail.com>
2025-01-08 12:29:48 +0300

    add correct Method равнобедренный треугольник

---
+++ src/main/java/by/itacademy/nikolaipsekha/Main.java
@@ -0,0 +1,9 @@
+package by.itacademy.nikolaipsekha;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Triangles.defineTriangle(4,4,2));
+        System.out.println(Triangles.defineTriangle(0,8,9));
+        System.out.println(Triangles.defineTriangle(4,8,9));
+    }
+}
