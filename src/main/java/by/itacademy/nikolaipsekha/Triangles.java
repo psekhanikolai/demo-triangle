d838350c32f76cb8c89a13d0e47d2b4e9237f2bf
Nikolai <n91056379@gmail.com>
2025-01-08 12:29:58 +0300

    add correct Method равнобедренный треугольник

--- src/main/java/by/itacademy/nikolaipsekha/Triangles.java
+++ src/main/java/by/itacademy/nikolaipsekha/Triangles.java
@@ -3,13 +3,13 @@ package by.itacademy.nikolaipsekha;
 public class Triangles {
     public static String defineTriangle(int a, int b, int c) {
         if (a + b < c || a + c < b || b + c < a) {
-            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c +  "не является треугольником";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c +  " не является треугольником";
         }
         if (a == b & b == c) {
-            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + "- равносторонний треугольник";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + " - равносторонний треугольник";
 
         } else if (a == b || a == c || b == c) {
-            return "Треугольник a=..., b=..., c=... - равнобедренный треугольник";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + " - равнобедренный треугольник";
         } else if (a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
             return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + " - прямогульный треугольник";
         }
