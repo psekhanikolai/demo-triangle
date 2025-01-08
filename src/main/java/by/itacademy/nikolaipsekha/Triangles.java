c883468b63cdfd31d4aaedfe4869ec2e1879871a
Nikolai <n91056379@gmail.com>
2025-01-08 12:43:52 +0300

    add correct Method не является треугольником

--- src/main/java/by/itacademy/nikolaipsekha/Triangles.java
+++ src/main/java/by/itacademy/nikolaipsekha/Triangles.java
@@ -3,7 +3,7 @@ package by.itacademy.nikolaipsekha;
 public class Triangles {
     public static String defineTriangle(int a, int b, int c) {
         if (a + b < c || a + c < b || b + c < a) {
-            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c +  " не является треугольником";
+            return "Треугольник a=" + a + ", b= " + b +  ", c= " + c +  " - не является треугольником";
         }
         if (a == b & b == c) {
             return "Треугольник a=" + a + ", b= " + b +  ", c= " + c + " - равносторонний треугольник";
